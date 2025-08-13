64ef455209a08efdfa3b1f2d8ef079b103f6bde7
Ulises <ulises0610c@gmail.com>
2025-08-13 12:12:19 -0700

    fix(android): align play-services-location to 21.3.0, API 35, metro blockList, notifee repos

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,17 @@
+// metro.config.js
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const config = {
+  resolver: {
+    blockList: exclusionList([
+      // carpetas temporales de CMake/Gradle que provocan ENOENT en Windows
+      /android[\/\\]\.cxx[\/\\].*/,
+      /android[\/\\]build[\/\\].*/,
+      /ios[\/\\]build[\/\\].*/,
+      /node_modules[\/\\]react-native-reanimated[\/\\]android[\/\\]\.cxx[\/\\].*/,
+    ]),
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
